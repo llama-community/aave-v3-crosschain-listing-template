c11f986a2f1802c4b408f694ad6a051824ee9d07
efecarranza <fcarr004@fiu.edu>
2023-08-11 11:08:53 -0400

    feat: updates

--- src/AaveV3_Eth_AssetsBridge_20230808/AaveV3_Eth_AssetsBridge_20230808.t.sol
+++ src/AaveV3_Eth_AssetsBridge_20230808/AaveV3_Eth_AssetsBridge_20230808.t.sol
@@ -5,33 +5,15 @@ import 'forge-std/Test.sol';
 import {GovHelpers} from 'aave-helpers/GovHelpers.sol';
 import {AaveGovernanceV2} from 'aave-address-book/AaveGovernanceV2.sol';
 import {AaveV3Ethereum, AaveV3EthereumAssets} from 'aave-address-book/AaveV3Ethereum.sol';
-import {ProtocolV3TestBase, ReserveConfig} from 'aave-helpers/ProtocolV3TestBase.sol';
-import {AaveV3_Eth_AssetsBridge_20230808} from './AaveV3_Eth_AssetsBridge_20230808.sol';
 
 /**
  * @dev Test for AaveV3_Eth_AssetsBridge_20230808
  * command: make test-contract filter=AaveV3_Eth_AssetsBridge_20230808
  */
-contract AaveV3_Eth_AssetsBridge_20230808_Test is ProtocolV3TestBase {
+contract AaveV3_Eth_AssetsBridge_20230808_Test is Test {
   function setUp() public {
     vm.createSelectFork(vm.rpcUrl('mainnet'), 17870364);
   }
 
-  function testProposalExecution() public {
-    AaveV3_Eth_AssetsBridge_20230808 proposal = new AaveV3_Eth_AssetsBridge_20230808();
 
-    ReserveConfig[] memory allConfigsBefore = createConfigurationSnapshot(
-      'preAaveV3_Eth_AssetsBridge_20230808',
-      AaveV3Ethereum.POOL
-    );
-
-    GovHelpers.executePayload(vm, address(proposal), AaveGovernanceV2.SHORT_EXECUTOR);
-
-    ReserveConfig[] memory allConfigsAfter = createConfigurationSnapshot(
-      'postAaveV3_Eth_AssetsBridge_20230808',
-      AaveV3Ethereum.POOL
-    );
-
-    diffReports('preAaveV3_Eth_AssetsBridge_20230808', 'postAaveV3_Eth_AssetsBridge_20230808');
-  }
 }
